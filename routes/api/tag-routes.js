37c6dea6d8e1bef8db776289f557e9db77d647d4
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-06-06 18:04:43 -0500

    update tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,19 +6,46 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  const tagData = await Tag.findAll({
+    include: {
+      model: Product,
+      attributes: ["product_name", "price", "stock", "category_id"],
+    },
+  });
+  return res.json(tagData);
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const tagData = await Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Product,
+      attributes: ["product_name", "price", "stock", "category_id"],
+    },
+  });
+  return res.json(tagData);
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  const tagData = await Tag.create({
+    tag_name: req.body.tag_name,
+  });
+  return res.json(tagData);
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  const tagData = await Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
+  return res.json(tagData);
 });
 
 router.delete('/:id', (req, res) => {
